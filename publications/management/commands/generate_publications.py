7d38d598f08d997e180286726c39c76f31462ac6
viktor <vktr.buyukli@gmail.com>
2023-05-20 19:20:45 +0300

    django command: insert publications into db

---
+++ publications/management/commands/generate_publications.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from publications.models import Publication
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("publications_count", nargs="?", type=int, default=10)
+
+    def handle(self, *args, publications_count, **options):
+        faker = Faker()
+        publications = [
+            Publication(
+                title=faker.sentence().replace(".", ""),
+                content=faker.text(),
+            )
+            for _ in range(publications_count)
+        ]
+
+        Publication.objects.bulk_create(publications)
+
+        self.stdout.write(
+            self.style.SUCCESS("Successfully created %s publications" % publications_count)
+        )
